e9b018c4bb89a1bdec316b022646976f4f7a4e9f
nali-kang <softnali92@gmail.com>
2024-02-19 22:17:58 +0900

    fix(path): 7주차 folder > shared로 수정

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import MainPage from "./pages/MainPage";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
 import FolderPage from "./pages/FolderPage";
+import SharedPage from "./pages/SharedPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<BaseLayout />}>
-          <Route exact path="/share" element={<MainPage />}></Route>
+          <Route exact path="/" element={<MainPage />}></Route>
+          <Route exact path="/shared" element={<SharedPage />}></Route>
           <Route exact path="/folder" element={<FolderPage />}></Route>
         </Route>
         <Route element={<SigninPage />} path="/signin" />
---
+++ src/pages/SharedPage.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import styles from "../styles/FolderPage.module.css";
+import { getRequestApi } from "../utils/requestApi";
+import LinkCardComponent from "../components/LinkCardComponent";
+import { useRequest } from "../hooks/useRequest";
+import FolderButton from "../components/FolderButton";
+
+const SharedPage = () => {
+  const [folderInfo, setFolderInfo] = useState({});
+
+  const { data, request } = useRequest({
+    url: "api/sample/folder",
+    method: "GET",
+  });
+
+  useEffect(() => {
+    request();
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      setFolderInfo(data);
+    }
+  }, [data]);
+
+  return (
+    <>
+      <section className={styles.folder_main_section}>
+        <img
+          src={folderInfo?.folder?.owner?.profileImageSource ?? ""}
+          className={styles.folder_user_img}
+        />
+        <p className={styles.folder_user_name}>
+          {folderInfo?.folder?.owner?.name ?? ""}
+        </p>
+        <strong className={styles.folder_name}>
+          {folderInfo?.folder?.name ?? ""}
+        </strong>
+      </section>
+      <section className={styles.folder_info_section}>
+        <div className={styles.folder_search_input_box}>
+          <img src="/images/Search.svg" />
+          <input
+            className={styles.folder_search_input}
+            placeholder="링크를 검색해 보세요"
+          />
+        </div>
+
+        {folderInfo?.folder?.links ? (
+          <article className={styles.folder_card_body}>
+            {folderInfo?.folder?.links?.map((e) => {
+              return (
+                <LinkCardComponent
+                  imgSrc={e.imageSource}
+                  createdAt={e.createdAt}
+                  desc={e.description}
+                  url={e.url}
+                />
+              );
+            })}
+          </article>
+        ) : (
+          <></>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default SharedPage;
